4d38f4d77ccf2b5da45d50e561a8312a326c489f
zhangyu <zhangyu@Oracle2.(none)>
2013-09-20 15:54:59 +0800

    oracle 自动安装脚本

--- oracle_yum.sh
+++ oracle_yum.sh
@@ -1,24 +1,47 @@
-yum install yum-fastestmirror -y
-yum install compat-db*
-yum install compat-libc*
-yum install compat-gcc*
-yum install libXp.so.6
-yum install libc-*
-yum install libaio*
-yum install openmotif
-yum install glibc-devel*
-yum install libgcc*
-yum install gnome-lib* 
-
-#zcat /tmp/10201_database_linux_x86_64.cpio.gz /data/setupfiles/Oracle/ | cpio -idmv
+#yum -y install yum-fastestmirror  compat-db* compat-libc* compat-gcc* libXp.so.6 libc-* libaio* openmotif glibc-devel* libgcc* gnome-lib* 
 
 cat > /etc/redhat-release << EOF
 Red Hat Enterprise Linux AS release 3 (Taroon)
-EOF 
+EOF
 
-echo >> ~/.bash_profile << EOF
-export ORACLE_BASE=/data1/oracle
-export ORACLE_HOME=/data1/oracle
+echo "add oracle user"
+groupadd oinstall
+groupadd dba
+useradd -m -g oinstall -G dba oracle
+passwd oracle
+
+mkdir -p /data/oracle/product
+mkdir -p /data/oracle/product/OraHome
+mkdir -p /data/oraInventory
+mkdir -p /data/oracle/oradata
+mkdir -p /var/opt/oracle
+
+chown -R oracle.oinstall /data/oracle
+chown -R oracle.oinstall /data/oracle/oradata
+chown -R oracle.oinstall /data/oracle/product/OraHome
+chown -R oracle.dba /data/oraInventory
+chown oracle.dba /var/opt/oracle 
+chmod -R 775 /data/oracle
+chmod -R 755 /var/opt/oracle
+
+
+echo "add limits.conf"
+cat >> /etc/security/limits.conf << EOF
+oracle soft nproc 2047
+oracle hard nproc 16384
+oracle soft nofile 1024
+oracle hard nofile 65536
+EOF
+
+cat >> /etc/pam.d/login << EOF
+session required /lib/security/pam_limits.so
+session required pam_limits.so
+EOF
+
+echo "add .bash_profile"
+cat >> /home/oracle/.bash_profile << EOF
+export ORACLE_BASE=/data/oracle
+export ORACLE_HOME=/data/oracle/product/OraHome
 export ORACLE_SID=orcl
 export ORACLE_TERM=xterm
 export PATH=$ORACLE_HOME/bin:$PATH
@@ -26,12 +49,9 @@ export PATH=$ORACLE_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib
 export CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
 
-if [ $USER = "oracle" ]; then
-  if [ $SHELL = "/bin/ksh" ]; then
-    ulimit -p 16384
-    ulimit -n 65536
-  else
-    ulimit -u 16384 -n 65536
-  fi
-fi
+ulimit -u 16384 -n 65536
+EOF
+
+cat >> /etc/hosts << EOF
+127.0.0.1 localhost
 EOF
